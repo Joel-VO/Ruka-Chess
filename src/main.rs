b23931b92750596653b3620321ecd1561e953149
joelvargheseoommen <joelvo9694@gmail.com>
2024-12-31 22:44:26 +0530

    a bug caused by fen notation denoting bishops and blacks turn to move as one and the same

--- src/main.rs
+++ src/main.rs
@@ -3,25 +3,24 @@ mod evaluation;
 use std::str::FromStr;
 use chess::Board;
 use crate::evaluation::alpha_beta::best_move;
+use crate::evaluation::evaluations::evaluate;
 
 
 fn main() {
-    let fen = "3Q4/5ppk/2q1p2p/2b1PP2/2p1R1P1/r1N5/7P/2R4K b - - 0 1";
+    let fen = "rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq - 0 1";
     match Board::from_str(fen) {
         Ok(board) => {
-            if let Some(mov) = best_move(&board, false, 4) {
-                println!("best move is {mov}");
-            } else {
-                println!("No moves available");
-            }
-            // println!("comment me out after testing");
-            // let moves = moves_sorted(&board);
-            // for pos_move in moves{
-            //     println!("{pos_move}");
+            // if let Some(mov) = best_move(&board, true, 2) {
+            //     println!("best move is {mov}");
+            // } else {
+            //     println!("No moves available");
             // }
+            let eval = evaluate(&board);
+            println!("{eval}");
         }
         Err(err) => {
             println!("error in fen : {err}");
         }
     }
+
 }
