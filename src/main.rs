220627d7b8982c24eab511d68306d80f518ed498
joelvargheseoommen <joelvo9694@gmail.com>
2025-01-15 19:51:24 +0530

    added in logic for computing hash for the piece at a position. Need to add in castling and en passant files

--- src/main.rs
+++ src/main.rs
@@ -4,31 +4,30 @@ mod search;
 use std::str::FromStr;
 use chess::Board;
 use crate::search::alpha_beta::best_move;
-// use crate::evaluation::evaluations::pe_sto;
-
+use crate::search::search_improvements::zobrist_hash::ZobristHashing;
 fn main() {
-    let fen = "8/1p2k3/1p2p3/4p3/2P1Pbp1/8/1P6/3K3B b - - 0 58"; //feed the fen to this...
-    //checks whose turn it is currently and feeds to alpha beta
-    let board_fen:Vec<&str> = fen.split_whitespace().collect();
-    let piece_to_move = board_fen[1];//takes just the current player
-
-        match Board::from_str(fen) {
-        Ok(board) => {//checks condition to see current player if board is legal
-            let is_maximising = if piece_to_move == "b"{
-                false
-            }else{
-                true
-            };
-            if let Some(mov) = best_move(&board, is_maximising, 8) {
-                println!("best move is {mov}");
-            } else {
-                println!("No moves available");
-            }
-            // let eval = pe_sto(&board);
-            // println!("{eval}")
-        }
-        Err(err) => {
-            println!("error in fen : {err}");//can be changed later to make sure errors are handled
-        }
-    }
+    // let fen = "8/1p2k3/1p2p3/4p3/2P1Pbp1/8/1P6/3K3B b - - 0 58"; //feed the fen to this...
+    // //checks whose turn it is currently and feeds to alpha beta
+    // let board_fen:Vec<&str> = fen.split_whitespace().collect();
+    // let piece_to_move = board_fen[1];//takes just the current player
+    //
+    //     match Board::from_str(fen) {
+    //     Ok(board) => {//checks condition to see current player if board is legal
+    //         let is_maximising = if piece_to_move == "b"{
+    //             false
+    //         }else{
+    //             true
+    //         };
+    //         if let Some(mov) = best_move(&board, is_maximising, 8) {
+    //             println!("best move is {mov}");
+    //         } else {
+    //             println!("No moves available");
+    //         }
+    //     }
+    //     Err(err) => {
+    //         println!("error in fen : {err}");//can be changed later to make sure errors are handled
+    //     }
+    // }
+    let zobrist_table = ZobristHashing::new_table();
+    println!("{0}",zobrist_table.piece_square[0][0])
 }
--- src/search/search_improvements/zobrist_hash.rs
+++ src/search/search_improvements/zobrist_hash.rs
@@ -1,6 +1,8 @@
 //add in zobrist hashing to speed up move selection by storing previously reached positions
 // will speed up the search by a good margin in theory
-use rand::Rng;
+
+use chess::{Board, Piece, Square};
+use rand::{thread_rng, Rng};
 
 const NUM_SQUARES:usize = 64;
 const PIECE_NO:usize = 12;//12 pieces
@@ -8,16 +10,43 @@ const CASTLING_RIGHTS:usize = 16;// 4*4 possible castles position
 const EN_PASSANT:usize = 8;// 8 files for castling
 const DEPTH:usize = 0;// depth factor that's checked...used for replacement strategy.
 
-struct ZobristHashing{
-    piece_square: [[u64;NUM_SQUARES];PIECE_NO],
-    castling_rights: [u64; CASTLING_RIGHTS],
-    en_passant_files: [u64; EN_PASSANT],
-    depth_factor: u64,
+pub struct ZobristHashing{
+    pub piece_square: [[u64;NUM_SQUARES];PIECE_NO],
+    pub castling_rights: [u64; CASTLING_RIGHTS],
+    pub en_passant_files: [u64; EN_PASSANT],
+    pub depth_factor: u64,
 }
-impl ZobristHashing {
-    fn new_rand()-> Self{
+impl ZobristHashing { //generates a random hash number every time its called and this is used as a
+    // key to compute the respective hash value. Use of 64 bits means collisions are 1 in a 100 million or so
+    pub fn new_table()-> Self{
+        let mut rng = thread_rng();
         Self{
-            
+            piece_square:[[0;NUM_SQUARES];PIECE_NO].map(|row| row.map(|_| rng.gen())),
+            castling_rights: [0; CASTLING_RIGHTS].map(|_| rng.gen()),
+            en_passant_files: [0; EN_PASSANT].map(|_| rng.gen()),
+            depth_factor: 0,
         }
     }
+}
+
+fn compute_hash_value(board:&Board, zobrist_key:&ZobristHashing) -> u64{
+    let mut hash:u64 = 0;
+
+    // add logic for each piece
+
+    for square in 0..NUM_SQUARES{
+        let position = unsafe {Square::new(square as u8)};
+        //could have used file and rank function, but might have been slower due to extra computation
+        // this is unsafe but only because of values greater than 64, which won't be violated here
+        if let Some(piece) = board.piece_on(position){// to check if such a piece exists
+            let piece_val:usize = piece.to_index();
+            hash^=zobrist_key.piece_square[square][piece_val];
+        }
+    }
+
+    //add logic for castling
+    //add logic for en passant
+
+
+    hash
 }
