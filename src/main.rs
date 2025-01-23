163335e9d9feef8e9b4f867e3b8b9559eb0947f3
joelvargheseoommen <joelvo9694@gmail.com>
2025-01-23 23:48:13 +0530

    completed quiescent_search.rs logic.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 mod evaluation;
 mod search;
 
+use std::path::is_separator;
 use std::str::FromStr;
 use chess::{Board};
 use crate::search::alpha_beta::best_move;
 use crate::search::search_improvements::quiescent_search::quiescent_search;
 use crate::evaluation::evaluations::pe_sto;
 fn main() {
-    let fen = "rnb1kb1r/ppp3pp/3Ppp2/3p4/3P4/8/PPP1P1PP/RNB1KBNR b KQkq - 0 8"; //feed the fen to this...
+    let fen = "rnbqkbnr/ppp1pppp/8/8/2pP4/1Q6/PP2PPPP/RNB1KBNR b KQkq - 1 3"; //feed the fen to this...
     //checks whose turn it is currently and feeds to alpha beta
     let board_fen:Vec<&str> = fen.split_whitespace().collect();
     let piece_to_move = board_fen[1];//takes just the current player
@@ -32,7 +33,7 @@ fn main() {
             //     let hash = updated_hash_move(current_hash, &mv, &zobrist_table, &board);
             //     println!("{hash}");
             // }
-            let search = quiescent_search(&board, i32::MIN, i32::MAX, 0, 3);
+            let search = quiescent_search(&board, i32::MIN, i32::MAX, 0, 3,false);
             println!("{search}");
             let eval = pe_sto(&board);
             println!("{eval}")
--- src/search/search_improvements/quiescent_search.rs
+++ src/search/search_improvements/quiescent_search.rs
@@ -6,6 +6,7 @@ use chess::{Board, ChessMove, MoveGen};
 use crate::evaluation::evaluations::pe_sto;
 
 fn tactical_moves(board: &Board)->Vec<ChessMove>{
+    ///returns a Vec<ChessMove> of all legal captures in a position. Can be modified to add checks as well to improve search quality.
     let move_gen = MoveGen::new_legal(board);
     let mut avail_moves:Vec<ChessMove> = Vec::new();
     for moves in move_gen {
@@ -16,32 +17,60 @@ fn tactical_moves(board: &Board)->Vec<ChessMove>{
     avail_moves
 }
 
-pub fn quiescent_search(board: &Board, mut alpha:i32, beta:i32, depth:u8, max_depth:u8) ->i32{//not working as intended... this is
-    // right now performing like a standard pesto eval.
-    if depth >= max_depth{//greater than or equal to, to prevent un stoppable search extension in case of some error.
+pub fn quiescent_search(board: &Board, mut alpha:i32, mut beta:i32, depth:u8, max_depth:u8, is_maximising:bool) ->i32{
+
+    let moves_tactical = tactical_moves(board);
+
+    if depth >= max_depth || moves_tactical.len()==0{
+        //greater than or equal to, to prevent un stoppable search extension in case of some error.
+        // This part was overlooked in the CPW pseudocode(according to what i read) and this is necessary to prevent moves that are egregious...
         return pe_sto(board);
     }
+
     let stand_pat:i32 = pe_sto(board);
-    let mut best_val:i32 = stand_pat;
-    if stand_pat >= beta{
-        return stand_pat;
+
+    //https://www.chessprogramming.org/Quiescence_Search
+
+    if is_maximising{//performing standard alpha beta init
+        if stand_pat >= beta{
+            return stand_pat //fail-soft and fail hard condition
+        }
+        if stand_pat > alpha{
+            alpha = stand_pat
+        }
+    }else{
+        // reverses all that's done in the above statements... as negamax pseudocode was given in CPW.
+        // the reverse should work for regular alpha beta... as it does according to this code...
+        // the exact workings are beyond me but got the general idea of a search window using stand_pat and beta as a window.
+        if stand_pat <= alpha{
+            return stand_pat
+        }
+        if stand_pat < beta{
+            beta = stand_pat
+        }
     }
-    if stand_pat > alpha{
-        alpha = stand_pat;
-    }
-    let moves_tactical = tactical_moves(board);
+
+
+    let mut best_val = if is_maximising{
+        // val init for best_val. Standard alpha beta style
+        i32::MIN
+    }else{
+        i32::MAX
+    };
+
     for moves in moves_tactical{
-        println!("{moves}");
+        // println!("{moves}");
         let current_position = board.make_move_new(moves);
-        let eval:i32 = quiescent_search(&current_position, -beta, -alpha, depth+1, max_depth);
-        if eval>=beta{
-            return eval;
+        let eval:i32 = quiescent_search(&current_position, alpha, beta, depth+1, max_depth, !is_maximising);
+        if is_maximising{//alpha beta updation
+            best_val = best_val.max(eval);
+            alpha = alpha.max(eval);
+        }else{
+            best_val = best_val.min(eval);
+            beta = beta.min(eval);
         }
-        if eval > best_val{
-            best_val = eval;
-        }
-        if eval > alpha{
-            alpha = eval;
+        if beta<=alpha{
+            break
         }
     }
     best_val
